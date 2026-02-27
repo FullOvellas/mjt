807e8323bed42b425c21946e32b8e1bbd1f5141c
Sergio Alonso <alo_se.r.g.io@proton.me>
2026-02-27 18:00:42 +0100

    Add separate subcommands for formatting and validation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use clap_stdin::MaybeStdin;
 use mjl::JsonLexer;
-use mjp::parse;
+use mjp::{Json, parse};
 use std::error::Error;
 
 use crate::emitting::emit;
@@ -10,27 +10,42 @@ mod emitting;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-pub struct Args {
-    /// JSON input. If substituted by `-`, it will be read from stdin
-    pub input: MaybeStdin<String>,
-    #[arg(short = 'i', long, default_value_t = 2)]
-    pub indentation: u8,
-    /// Output reformatted JSON
-    #[arg(short = 'p', long, default_value_t = true)]
-    pub pretty: bool,
+struct Args {
+    #[command(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(clap::Subcommand)]
+enum Subcommand {
+    /// Apply formatting to the input
+    Format {
+        /// JSON input. If substituted by `-`, it will be read from stdin
+        input: MaybeStdin<String>,
+        /// Amount of spaces used for indentation
+        #[arg(short = 'i', long, default_value_t = 2)]
+        indentation: u8,
+    },
+    /// Aarse the input and fail if it's invalid
+    Validate {
+        /// JSON input. If substituted by `-`, it will be read from stdin
+        input: MaybeStdin<String>,
+    },
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let parsed = parse(JsonLexer {
-        input: &args.input,
-        byte_offset: 0,
-    })?;
-    if args.pretty {
-        println!("{}", emit(parsed, args.indentation));
-        Ok(())
-    } else {
-        println!("{}", args.input);
-        Ok(())
+    match args.subcommand {
+        Subcommand::Format { input, indentation } => {
+            println!("{}", emit(do_parse(&input)?, indentation));
+            Ok(())
+        }
+        Subcommand::Validate { input } => do_parse(&input).map(|_| ()),
     }
+}
+
+fn do_parse(input: &MaybeStdin<String>) -> Result<Json, Box<dyn Error>> {
+    parse(JsonLexer {
+        input,
+        byte_offset: 0,
+    })
 }
